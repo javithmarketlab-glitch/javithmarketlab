63c21b2fa8e1dc18a21ba475ba12542b6f4356a4
thow654321 <mohamedthowfeek419@gmail.com>
2026-08-06 21:18:14 +0530

    fix db ssl

--- backend/db.js
+++ backend/db.js
@@ -5,14 +5,12 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  port: process.env.DB_PORT || 3306,
+  ssl: false
 });
 
 db.connect((err) => {
-  if (err) {
+  if(err){
     console.log("Database Error:", err);
   } else {
     console.log("MySQL Connected ✅");
